3a85e10abda750776cc353c2d431cb12aff6b323
Jack-X-Yang <1487019521@qq.com>
2015-09-28 19:58:17 +0800

    添加注释

--- app/src/util/HandlebarsHelper.js
+++ app/src/util/HandlebarsHelper.js
@@ -1,16 +1,17 @@
 define(['handlebars', 'text!templates/navbar.hbs'], function (Handlebars, navbarTemplate) {
-            var handlebarsID = 0;
+    //自定义handlebars helper
+    var handlebarsID = 0;
 
-            Handlebars.registerHelper('a-link-id', function () {
-                handlebarsID++;
-                return new Handlebars.SafeString('<a data-toggle="collapse" data-parent="#accordion" href="#' + handlebarsID + '">');
-            });
+    Handlebars.registerHelper('a-link-id', function () {
+        handlebarsID++;
+        return new Handlebars.SafeString('<a data-toggle="collapse" data-parent="#accordion" href="#' + handlebarsID + '">');
+    });
 
-            Handlebars.registerHelper('div-id', function () {
-                return new Handlebars.SafeString('<div id="' + handlebarsID + '" class="panel-collapse collapse in">');
-            });
-            Handlebars.registerHelper('navbar', function () {
-                var template = Handlebars.compile(navbarTemplate);
-                return new Handlebars.SafeString(template());
-            });
+    Handlebars.registerHelper('div-id', function () {
+        return new Handlebars.SafeString('<div id="' + handlebarsID + '" class="panel-collapse collapse in">');
+    });
+    Handlebars.registerHelper('navbar', function () {
+        var template = Handlebars.compile(navbarTemplate);
+        return new Handlebars.SafeString(template());
+    });
 });
