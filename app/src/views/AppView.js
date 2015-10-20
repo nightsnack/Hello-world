88a40e6681f6f4e899d278ee0a1c38d4bd5d6cb0
Jack-X-Yang <1487019521@qq.com>
2015-10-20 11:47:32 +0800

    获取openid参数出错

--- app/src/views/AppView.js
+++ app/src/views/AppView.js
@@ -43,7 +43,7 @@ define([
             },
             getOpenid: function (param) {
                 //构造一个含有目标参数的正则表达式对象
-                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+                var reg = new RegExp("(^|&)" + param + "=([^&]*)(&|$)");
                 //匹配目标参数
                 var r = window.location.search.substr(1).match(reg);
                 //返回参数值
